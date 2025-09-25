c64d7f5bb35ea5dd86aa26c6892eec6b88d909f5
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-25 17:52:44 +0200

    Set up prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import '../css/App.css'
+import '../css/App.css';
 
 export function App() {
   return (
-      <div>
-        <h1>DAM</h1>
-        <p>Digital Asset Management</p>
-      </div>
-  )
+    <div>
+      <h1>DAM</h1>
+      <p>Digital Asset Management</p>
+    </div>
+  );
 }
