f5e6e3d2f12da0b629d5f71fd6469b74f5c68af6
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-25 15:44:09 +0200

    Use ISO 8601 dates in API responses

---
+++ backend/app/GraphQL/Scalars/DateTime.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GraphQL\Scalars;
+
+use Carbon\Carbon;
+use Nuwave\Lighthouse\Schema\Types\Scalars\DateTime as BaseDateTime;
+
+final class DateTime extends BaseDateTime
+{
+    protected function format(Carbon $carbon): string
+    {
+        return $carbon->utc()->format('Y-m-d\TH:i:s\Z');
+    }
+}
