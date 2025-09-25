d0164e712d6b1e0a2b35fca17a8b846a04a0de06
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-25 19:38:39 +0200

    Set up root route

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 import '../css/index.css';
-import { App } from './App.tsx';
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
