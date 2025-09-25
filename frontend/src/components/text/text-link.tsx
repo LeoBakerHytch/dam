a37bb9962fba172f77353a87ff7e9bd4dbb9a019
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-25 22:47:57 +0200

    Add TextLink component

---
+++ frontend/src/components/text/text-link.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from 'react';
+import { Link } from 'react-router';
+
+import { cn } from '@/lib/utils';
+
+type LinkProps = ComponentProps<typeof Link>;
+
+export default function TextLink({ className = '', children, ...props }: LinkProps) {
+  return (
+    <Link
+      className={cn(
+        'text-foreground hover:decoration-current! underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out dark:decoration-neutral-500',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
