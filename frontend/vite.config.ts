9849b59859b83a84ec5fe2a240f6f0ad7e746627
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-25 20:06:59 +0200

    Set up import alias (`@`) for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      '@': resolve(process.cwd(), './src'),
+    },
+  },
 
   esbuild: {
     jsx: 'automatic',
