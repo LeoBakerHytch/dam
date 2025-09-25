d84e4b0f42b4bc0bb88d5ed8bbb7fcd2bde17104
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-25 19:04:21 +0200

    Set up shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import '../css/App.css';
 
 export function App() {
   return (
-    <div>
-      <h1 className="text-purple-700">DAM</h1>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <h1 className="text-5xl text-purple-700">DAM</h1>
       <p>Digital Asset Management</p>
     </div>
   );
