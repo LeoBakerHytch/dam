77851527a1e920d97656e57497be0451112e272e
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-28 20:44:16 +0200

    Bake directory creation & ownership into the images

--- backend/docker/init.sh
+++ backend/docker/init.sh
@@ -2,21 +2,15 @@
 
 set -e
 
-mkdir -p bootstrap/cache
-mkdir -p storage/app
-mkdir -p storage/framework/cache
-mkdir -p storage/framework/sessions
-mkdir -p storage/framework/views
-mkdir -p storage/logs
+if [ "$APP_ENV" = "local" ]; then
+  if [ -d "public/storage" ] && [ ! -L "public/storage" ]; then
+    echo "Removing existing directory public/storage"
+    rm -rf public/storage
+  fi
 
-# Adjust ownership only in local/dev, not when on Fly volumes
-if [ "$APP_ENV" != "production" ]; then
-    chown -R www-data:www-data storage bootstrap/cache
-fi
-
-# Create storage symlink for serving uploaded files (local development only)
-if [ "$APP_ENV" = "local" ] && [ ! -L "public/storage" ]; then
+  if [ ! -e "public/storage" ]; then
     php artisan storage:link
+  fi
 fi
 
 # Hand off to environment-specific CMD
