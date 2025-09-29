564ee5cb797a4c67e2950062e053814e9eba948c
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-29 22:20:21 +0200

    Remove unnecessary checks that @guard already handles

--- backend/app/GraphQL/Resolvers/ImageAsset_Upload.php
+++ backend/app/GraphQL/Resolvers/ImageAsset_Upload.php
@@ -16,16 +16,7 @@ final class ImageAsset_Upload
     public function __invoke($_, array $args): array
     {
         $input = $args['input'];
-
-        $authenticatedUser = auth('api')->user();
-        if (!$authenticatedUser) {
-            throw new Exception('User not authenticated');
-        }
-
-        $user = User::find($authenticatedUser->getAuthIdentifier());
-        if (!$user) {
-            throw new Exception('User not found');
-        }
+        $user = auth('api')->user();
 
         /** @var UploadedFile $image */
         $image = $input['image'];
