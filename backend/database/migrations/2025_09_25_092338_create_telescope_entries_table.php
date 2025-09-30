add8c90c0e07c4ac9a57da0f27f77469cfd6ec2e
Leo Baker-Hytch <ltobh@tuta.com>
2025-09-30 09:57:53 +0200

    Fix telescope migration to run with current connection (only locally)

--- backend/database/migrations/2025_09_25_092338_create_telescope_entries_table.php
+++ backend/database/migrations/2025_09_25_092338_create_telescope_entries_table.php
@@ -6,22 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Get the migration connection name.
-     */
-    public function getConnection(): ?string
-    {
-        return config('telescope.storage.database.connection');
-    }
-
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        $schema = Schema::connection($this->getConnection());
+        if ( ! app()->environment('local')) {
+            return;
+        }
 
-        $schema->create('telescope_entries', function (Blueprint $table) {
+        Schema::create('telescope_entries', function (Blueprint $table) {
             $table->bigIncrements('sequence');
             $table->uuid('uuid');
             $table->uuid('batch_id');
@@ -38,33 +29,33 @@ return new class extends Migration
             $table->index(['type', 'should_display_on_index']);
         });
 
-        $schema->create('telescope_entries_tags', function (Blueprint $table) {
+        Schema::create('telescope_entries_tags', function (Blueprint $table) {
             $table->uuid('entry_uuid');
             $table->string('tag');
 
             $table->primary(['entry_uuid', 'tag']);
             $table->index('tag');
 
-            $table->foreign('entry_uuid')
+            $table
+                ->foreign('entry_uuid')
                 ->references('uuid')
                 ->on('telescope_entries')
                 ->onDelete('cascade');
         });
 
-        $schema->create('telescope_monitoring', function (Blueprint $table) {
+        Schema::create('telescope_monitoring', function (Blueprint $table) {
             $table->string('tag')->primary();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        $schema = Schema::connection($this->getConnection());
+        if ( ! app()->environment('local')) {
+            return;
+        }
 
-        $schema->dropIfExists('telescope_entries_tags');
-        $schema->dropIfExists('telescope_entries');
-        $schema->dropIfExists('telescope_monitoring');
+        Schema::dropIfExists('telescope_entries_tags');
+        Schema::dropIfExists('telescope_entries');
+        Schema::dropIfExists('telescope_monitoring');
     }
 };
